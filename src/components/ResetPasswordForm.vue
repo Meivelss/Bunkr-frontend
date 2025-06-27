c9d3208a36418261265b9fd928c085adf0ecaf21
krasnykid <tmisiak10m@gmail.com>
2025-06-27 23:54:58 +0200

    feature: reset password feedback

--- src/components/ResetPasswordForm.vue
+++ src/components/ResetPasswordForm.vue
@@ -3,32 +3,76 @@ import { ref } from "vue";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft } from "lucide-vue-next";
+import { ShieldCheck } from "lucide-vue-next";
+import { CircleX } from "lucide-vue-next";
 
+const loading = ref(false);
 const password = ref("");
 const repeatPassword = ref("");
+const error = ref("");
+const success = ref(false);
 
 /* called on form submit */
 async function handleSubmit() {
-  if (password.value !== repeatPassword.value) {
-    console.log("passwords do not match");
+  loading.value = true;
+  error.value = "";
+
+  try {
+    if (password.value !== repeatPassword.value) {
+      throw new Error("Hasła nie pasują do siebie");
+    }
+
+    /* TODO: make a more elaborate check here */
+    if (password.value.length < 8) {
+      throw new Error("Hasło jest za krótkie");
+    }
+
+    /* if all is good, we should display a happy popup saying the user can log in now */
+
+    /* take user to login page */
+    success.value = true;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error("Authentication failed:", err.message);
+      error.value = err.message;
+    } else {
+      const fallbackError = "Wystąpił nieoczekiwany błąd.";
+      console.error("An unexpected error occurred:", err);
+      error.value = fallbackError;
+    }
+  } finally {
+    loading.value = false;
+    repeatPassword.value = "";
   }
-
-  /* TODO: make a more elaborate check here */
-  if (password.value.length < 8) {
-    console.log("password is shit");
-  }
-
-  /* if all is good, we should display a happy popup saying the user can log in now */
-
-  /* take user to login page */
-  window.location.replace("/login");
 }
 </script>
 
 <template>
-  <div class="flex w-full flex-col space-y-4 select-none">
+  <div
+    v-if="success"
+    class="flex flex-col items-center justify-center gap-4 text-white"
+  >
+    <ShieldCheck color="white" :stroke-width="1" :size="102" />
+    <div class="flex flex-col gap-2 text-center">
+      <p>
+        Reset hasła powiódł się, możesz teraz zalogować się swoim nowym hasłem.
+      </p>
+    </div>
+    <a href="/login" class="flex items-center gap-1 text-white hover:underline">
+      <ChevronLeft color="white" />
+      <span>Powrót do logowania</span>
+    </a>
+  </div>
+  <div v-else class="flex w-full flex-col space-y-4 select-none">
     <div class="flex w-full">
       <h1 class="text-4xl font-bold text-white">Nowe hasło</h1>
+    </div>
+    <div
+      v-if="error"
+      class="flex w-full flex-col items-center justify-center gap-2 rounded-md border-1 border-red-900 bg-red-800/40 p-3 text-sm font-bold text-white shadow-md backdrop-blur-sm"
+    >
+      <CircleX :size="20" />
+      <p class="text-center">{{ error }}</p>
     </div>
     <p class="text-sm text-white">
       Ustal nowe hasło, tym razem nie zapomnij B)
