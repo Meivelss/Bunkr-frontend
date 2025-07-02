59112c0882f7cc9953ebfde09fa0d7806b828ec1
krasnykid <tmisiak10m@gmail.com>
2025-07-02 23:53:12 +0200

    feature: breadcrumb

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { Slash } from "lucide-vue-next";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+</script>
+
+<template>
+  <Breadcrumb>
+    <BreadcrumbList class="font-body text-xl">
+      <!-- <BreadcrumbItem>
+        <BreadcrumbLink href="/"> Home </BreadcrumbLink>
+      </BreadcrumbItem> -->
+      <BreadcrumbItem>
+        <BreadcrumbPage>Fredi</BreadcrumbPage>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator>
+        <Slash />
+      </BreadcrumbSeparator>
+      <BreadcrumbItem>
+        <BreadcrumbPage>Kamionka</BreadcrumbPage>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator>
+        <Slash />
+      </BreadcrumbSeparator>
+      <BreadcrumbItem>
+        <BreadcrumbPage>Gmina</BreadcrumbPage>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator>
+        <Slash />
+      </BreadcrumbSeparator>
+      <BreadcrumbItem>
+        <BreadcrumbPage>Burzenin</BreadcrumbPage>
+      </BreadcrumbItem>
+    </BreadcrumbList>
+  </Breadcrumb>
+</template>
