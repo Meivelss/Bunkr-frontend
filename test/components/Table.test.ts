91953873f17c8baa39b02ef7a82c82b32a31c841
Michalina Lasek <michalina.lasek@gmail.com>
2025-07-07 14:12:41 +0200

    hotfix: minor letters case changes

--- test/components/Table.test.ts
+++ test/components/Table.test.ts
@@ -1,15 +1,15 @@
 import { mount } from "@vue/test-utils";
 import TableComponent from "@/components/Table.vue";
 
-describe("TableComponent mounts properly", () => {
+describe("TableComponent tests", () => {
   const wrapper = mount(TableComponent);
-  it("Should mount the table conponent", () => {
+  it("should mount the table conponent", () => {
     expect(wrapper.find("table").exists()).toBe(true);
   });
-  it("Should render at least one column", () => {
+  it("should render at least one column", () => {
     expect(wrapper.findAll("tbody tr").length).toBeGreaterThan(0);
   });
-  it("Should render the number of found records", () => {
+  it("should render the number of found records", () => {
     expect(wrapper.text()).toMatch(/\d wyników/);
   });
 });
